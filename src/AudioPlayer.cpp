4cf3affc578d5cf40c2766fdb5030579d57cacaf
Maisquasar <romainbou@icloud.com>
2025-01-02 18:45:59 +0100

    Initial Commit

---
+++ src/AudioPlayer.cpp
@@ -0,0 +1,157 @@
+﻿#include "AudioPlayer.h"
+
+#include <array>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#ifdef _WIN32
+#define popen _popen
+#define pclose _pclose
+#endif
+
+// Function to execute a shell command and capture its output
+std::string execCommand(const std::string& command) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+Sound::Sound(const std::string& name, const std::filesystem::path& path) : m_name(name), m_path(path), m_length(0)
+{
+}
+
+bool Sound::Load()
+{
+    std::ifstream audioFile(m_path, std::ios::in|std::ios::binary|std::ios::ate);
+    if (audioFile.is_open()) {
+        m_size = audioFile.tellg();
+        m_data = new uint8_t[m_size];
+        audioFile.seekg (0, std::ios::beg);
+        audioFile.read(reinterpret_cast<char*>(m_data), m_size);
+        audioFile.close();
+        return true;
+    }
+    else {
+        std::cout << "Failed to read audio file" << std::endl;
+        return false;
+    }
+}
+
+SoundManager* SoundManager::m_instance = nullptr;
+
+Sound* AudioPlayer::DownloadVideo(const std::string& url)
+{
+    if (url.empty()) {
+        std::cout << "No video provided" << std::endl;
+        return nullptr;
+    }
+
+    if (!std::filesystem::exists(YTP_DLP_PATH)) {
+        std::cout << "yt-dlp not found" << std::endl;
+        return nullptr;
+    }
+
+    if (!std::filesystem::exists(OUTPUT_PATH)) {
+        std::filesystem::create_directory(OUTPUT_PATH);
+    }
+
+    std::filesystem::path currentPath = std::filesystem::current_path();
+    std::string currentPathString = currentPath.generic_string();
+    std::filesystem::path fileOutputPath;
+    std::string videoTitle;
+#ifdef _WIN32
+    {
+        // Construct the yt-dlp command
+        std::string command = "cmd /C \"" + currentPathString + "/" YTP_DLP_PATH "\" --get-id " + url;
+
+        // Execute the command and capture the output
+        videoTitle = execCommand(command);
+
+        // Remove the trailing newline character
+        videoTitle.erase(videoTitle.find_last_not_of(" \t\r\n") + 1);
+
+        std::filesystem::path title = videoTitle;
+
+        fileOutputPath = (currentPath / OUTPUT_PATH / title).generic_string() + ".wav";
+    }
+#else
+    TODO
+#endif
+    
+    if (std::filesystem::exists(fileOutputPath.generic_string() + ".pcm")) {
+        std::cout << "Video already downloaded" << '\n';
+        Sound* outputSound = new Sound(videoTitle, fileOutputPath.generic_string() + ".pcm");
+        return outputSound;
+    }
+
+#ifdef _WIN32
+    std::string ffmpegLoc = "--ffmpeg-location " + currentPath.parent_path().generic_string() + "/libs/ffmpeg/bin";
+    std::string outputPath = "-o " + currentPathString + "/" OUTPUT_PATH + "%(id)s.%(ext)s";
+    std::string command = "cmd /C \"" + currentPathString + "/" YTP_DLP_PATH "\" " + ffmpegLoc + " --extract-audio --audio-format wav " + outputPath + " ";
+#else
+    TODO
+#endif
+    command += url;
+    
+    std::system(command.c_str());
+
+    // Convert to raw pcm
+    {
+        std::string outputPath = fileOutputPath.generic_string();
+        // 48kHz pcm 2 channels 16 bit
+        std::string ffmpegCommand = "cmd /C " + currentPath.parent_path().generic_string() + "/libs/ffmpeg/bin/ffmpeg.exe -i " + outputPath + " -acodec pcm_s16le -f s16le -ac 2 -ar 48000 " + outputPath + ".pcm";
+        std::system(ffmpegCommand.c_str());
+
+        std::remove(fileOutputPath.generic_string().c_str());
+
+        fileOutputPath = fileOutputPath.generic_string() + ".pcm";
+    }
+    Sound* outputSound = new Sound(videoTitle, fileOutputPath);
+
+    if (!std::filesystem::exists(fileOutputPath)) {
+        std::cout << "Failed to convert video " << fileOutputPath << " to pcm" << '\n';
+        return nullptr;
+    }
+
+    std::cout << "Video downloaded" << '\n';
+    
+    return outputSound;
+}
+
+bool AudioPlayer::PlayAudio(const dpp::cluster& bot, const dpp::interaction_create_t& event, Sound* sound)
+{
+    auto path = sound->GetPath();
+    if (!std::filesystem::exists(path)) {
+        std::cout << "Audio file " << path << " not found" << std::endl;
+        return false;
+    }
+    dpp::voiceconn* v = event.from->get_voice(event.command.guild_id);
+
+    auto timeout = std::chrono::system_clock::now() + std::chrono::seconds(20);
+    
+    while (!v || !v->voiceclient || !v->voiceclient->is_ready()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        if (std::chrono::system_clock::now() > timeout) {
+            std::cout << "Voice client not ready" << std::endl;
+            return false;
+        }
+    }
+    std::cout << "Voice client ready" << std::endl;
+    dpp::discord_voice_client* voiceClient = v->voiceclient;
+
+    sound->Load();
+
+    voiceClient->send_audio_raw(reinterpret_cast<uint16_t*>(sound->GetData()), sound->GetDataSize());
+
+    std::cout << "Playing audio" << '\n';
+    return true;
+}
---
+++ src/AudioPlayer.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include <cluster.h>
+#include <filesystem>
+#include <string>
+
+#define OUTPUT_PATH "audio/"
+
+#ifdef _WIN32
+#define YTP_DLP_PATH "../libs/yt-dlp/yt-dlp.exe"
+#else
+TODO
+#endif
+
+class Sound
+{
+public:
+    explicit Sound(const std::string& name, const std::filesystem::path& path);
+    ~Sound() = default;
+
+    bool Load();
+
+    std::filesystem::path GetPath() const { return m_path; }
+    std::string GetName() const { return m_name; }
+    uint32_t GetLength() const { return m_length; }
+
+    uint8_t* GetData() const { return m_data; }
+    size_t GetDataSize() const { return m_size; }
+private:
+    std::filesystem::path m_path;
+    std::string m_name;
+    uint32_t m_length;
+    size_t m_size = 0;
+    uint8_t* m_data = nullptr;
+};
+
+class SoundManager
+{
+public:
+    SoundManager() = default;
+    ~SoundManager() = default;
+
+    static SoundManager* GetInstance() { return m_instance; }
+
+    static void Initialize() { m_instance = new SoundManager(); }
+
+    static void Destroy() { delete m_instance; }
+
+    bool AddSound(const std::string& name, const std::filesystem::path& path) { return m_sounds[name] = new Sound(name, path); }
+
+    Sound* GetSound(const std::string& name) { return m_sounds[name]; }
+    std::map<std::string, Sound*> GetSounds() { return m_sounds; }
+private:
+    static SoundManager* m_instance;
+
+    std::map<std::string, Sound*> m_sounds;
+};
+
+class AudioPlayer
+{
+public:
+    static Sound* DownloadVideo(const std::string& url);
+
+    static bool PlayAudio(const dpp::cluster& bot, const dpp::interaction_create_t& event, Sound* sound);
+};
